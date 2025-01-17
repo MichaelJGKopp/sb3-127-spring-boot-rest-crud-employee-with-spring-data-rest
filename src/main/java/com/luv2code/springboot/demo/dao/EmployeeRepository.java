719eb7f2e8ff20850df4fd003d9e667a4168bf3b
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-17 19:11:58 +0100

    change endpoint from employees to workers

--- src/main/java/com/luv2code/springboot/demo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springboot.demo.dao;
 
 import com.luv2code.springboot.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
