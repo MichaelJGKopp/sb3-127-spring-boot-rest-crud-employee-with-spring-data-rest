0c443b8a7722f841f78b48cf57b0936fdc99ade9
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-17 19:12:18 +0100

    back to employees endpoint

--- src/main/java/com/luv2code/springboot/demo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/demo/dao/EmployeeRepository.java
@@ -2,9 +2,8 @@ package com.luv2code.springboot.demo.dao;
 
 import com.luv2code.springboot.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
